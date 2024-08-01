1a18065c14c78b1a07eedc328d74dbc6116fae65
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-08-01 11:23:01 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,51 @@
-#!/usr/bin/python3
-""" N queens """
+#!/usr/bin/env python3
 import sys
 
+def is_valid(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def solve_nqueens(N):
+    def backtrack(row):
+        if row == N:
+            solution = []
+            for r in range(N):
+                solution.append([r, board[r]])
+            solutions.append(solution)
+            return
+        for col in range(N):
+            if is_valid(board, row, col):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+    board = [-1] * N
+    solutions = []
+    backtrack(0)
+    return solutions
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-n = int(sys.argv[1])
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
 
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+if __name__ == "__main__":
+    main()
