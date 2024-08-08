f20d30789a9eba8ffd5cbe4599ca7bc692e68657
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-08-08 17:00:58 +0000

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,17 @@
 #!/usr/bin/node
 
-async function getMovieCharacters(movieId) {
-  const baseUrl = 'https://swapi.dev/api/films/';
+const request = require('request');
 
-  try {
-      // Fetch the film data
-      const response = await fetch(${baseUrl}${movieId}/);
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-      const filmData = await response.json();
-
-      // Extract the characters list
-      const charactersUrls = filmData.characters;
-
-      // Fetch and print each character's name
-      for (const url of charactersUrls) {
-          try {
-              const charResponse = await fetch(url);
-              if (!charResponse.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              const characterData = await charResponse.json();
-              console.log(characterData.name);
-          } catch (error) {
-              console.error('An error occurred while fetching character data:', error);
-          }
-      }
-  } catch (error) {
-      console.error('An error occurred:', error);
-  }
-}
-
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: node script.js <Movie ID>');
-  process.exit(1);
-}
-
-getMovieCharacters(movieId);
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
